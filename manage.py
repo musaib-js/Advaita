224141bd3f2d38bf0727a646f347df08b69e1241
Roshan Dash <roshan1337d@gmail.com>
2023-02-03 00:57:08 +0530

    ci/cd pipeline

--- manage.py
+++ manage.py
@@ -7,6 +7,8 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'advaita.settings')
+    from django.core.management.commands.runserver import Command as runserver
+    runserver.default_port = "5010"
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
